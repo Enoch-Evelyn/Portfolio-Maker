227ac59d553b2cead809129516e3ce65c1fbe4e1
Knock <enoch10evelyn@gmail.com>
2024-07-08 13:43:33 -0400

    Generate Html

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const title = profileDataArgs[0];
+const github = profileDataArgs[1];
 
 
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-      }
-      console.log('================');
+
+// const printProfileData = (profileDataArr) => {
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//       }
+//       console.log('================');
 
 
-      profileDataArr.forEach(profileItem => console.log(profileItem));
+//       profileDataArr.forEach(profileItem => console.log(profileItem));
   
-  };
+//   };
   
-  printProfileData(profileDataArgs);
+//   printProfileData(profileDataArgs);
+
+const generatePage = (userName, githubName) => {
+
+return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
+
+  <body>
+    <h1>${title}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+`;
+};
+console.log(title, github);
+console.log(generatePage(title,github));
 
   
